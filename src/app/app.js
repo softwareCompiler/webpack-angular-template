bb38cb0ee24aa9f4bfec6d6bf0859aa7ea73ffb2
Hao Qin <hqin@liyutech.com>
2016-04-03 22:51:09 -0500

    modified from https://github.com/preboot/angular-webpack.git

---
+++ src/app/app.js
@@ -0,0 +1,46 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+
+
+// necessary for css to work
+import '../style/app.css';
+// import '../../node_modules/font-awesome/css/font-awesome.min.css';
+
+require("font-awesome-webpack");
+
+// require('font-awesome');
+
+// install caching service worker
+import offline from 'offline-plugin/runtime'
+
+console.log('require sw runtime offline ...' + uiRouter);
+
+offline.install();
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+
+  constructor($scope) {
+    this.url = 'https://liyutech.com/fc';
+  }
+}
+
+// app.url = "https://liyutech.com/fc";
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [uiRouter])
+  .directive('app', app)
+  .config(function($stateProvider) {
+    console.log('class config $routeProvider ' + $stateProvider);
+
+  })
+.controller('AppCtrl', ['$scope', AppCtrl]);
+
+export default MODULE_NAME;
---
+++ webpack.config.prod.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var OfflinePlugin = require('offline-plugin');
+
+const WebpackShellPlugin = require('webpack-shell-plugin');
+
+const PROD_DOMAIN = 'domain';
+const PROD_ROOT_PATH = '/' + PROD_DOMAIN;
+const PreBuildTask = `ls -l ${PROD_DOMAIN} && rm -rf  ${PROD_DOMAIN} && ls -l ${PROD_DOMAIN}`;
+console.log('PreBuildTask ' + PreBuildTask);
+  
+module.exports = function makeWebpackConfig() {
+  var config = {};
+  config.entry = {
+    app: './src/app/app.js'
+  };
+
+  config.output = {
+    path: __dirname + PROD_ROOT_PATH,
+    publicPath: PROD_ROOT_PATH,
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[hash].js'
+  };
+
+  config.devtool = 'source-map';
+
+  config.module = {
+    preLoaders: [],
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')
+    }, {
+      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+      loader: 'file'
+    }, {
+      test: /\.html$/,
+      loader: 'raw'
+    }]
+  };
+  config.postcss = [
+    autoprefixer({
+      browsers: ['last 2 version']
+    })
+  ];
+
+
+
+  config.plugins = [];
+
+  config.plugins.push(
+    new WebpackShellPlugin({
+      onBuildStart: [PreBuildTask],
+      onBuildEnd: [`echo "Ending "${PROD_ROOT_PATH}`]
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/public/index.prod.html',
+      filename: 'index.html',
+      inject: 'body'
+    }),
+
+    new ExtractTextPlugin('[name].[hash].css', {
+      disable: false
+    }),
+
+    new webpack.NoErrorsPlugin(),
+
+    new webpack.optimize.DedupePlugin(),
+
+    new webpack.optimize.UglifyJsPlugin(),
+
+    new CopyWebpackPlugin(
+      [{from: __dirname + '/src/public'}], 
+      {
+      ignore: [{glob: '**/*.prod.html'}]
+    }),
+    new OfflinePlugin({
+      // All options are optional
+      caches: 'all',
+      scope: PROD_ROOT_PATH + '/',
+      updateStrategy: 'all',
+      version: 'v2',
+
+      ServiceWorker: {
+        output: 'sw.js'
+      },
+
+      AppCache: {
+        directory: 'appcache/'
+      }
+    })
+  )
+  return config;
+}();
