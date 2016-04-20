0aba490b574451e00eb6ae396764e2d2d503288d
Hao Qin <hqin@liyutech.com>
2016-04-19 20:58:14 -0500

    v0.0.9: added webpack-common.config.js shared by dev and prod config files

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Create a webpack template project which supports AngularJS 1.5, ES6 with Babel, RxJS, and service worker for offline experience. This project mainly consists of two controllers, one Angular directive,  and the corresponding views. The compiled applicaiton can be dropped to an HTTP server and served at a predefined domain.
 
+This project is inspired by the project at https://github.com/preboot/angular-webpack.git.
 <h3>Installation</h3>
 <i>```git clone git@github.com:liyutech/webpack-angular-template```</i>
 <p>
@@ -23,7 +24,7 @@ This will compile the application. The compiled files will be output to the doma
 <p>After the compilation is complete, copy the entire domain/ directory to the document root of an HTTP server. Notice that since service workers can only be run under the https protoocol, the HTTP server should be configured to support https.
 </p>
 <p>
-It is assumed the document root of the server is <i>/www</i>. To use a different document root, modify the default defined in webpack.config.prod.js. The deployed application can be accessed at https://localhost:8443/domain.
+It is assumed the document root of the server is <i>/www</i>. To use a different document root, modify the default defined in webpack.config.prod.js. The deployed application can be accessed at https://localhost:8443/domain. The login user name and password are Ada and Ada123, respectively.
 </p>
 Notice that if the browser does not support service workers, the application will fall back to appcache. 
 
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,100 +1,41 @@
 'use strict';
 
+
+const common = require('./webpack.common.config.js');
+
+const config = common.config;
+
 const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const OfflinePlugin = require('offline-plugin');
-
-const WebpackShellPlugin = require('webpack-shell-plugin');
-
-const PROD_DOMAIN = 'domain';
-const PROD_ROOT_PATH = '/' + PROD_DOMAIN;
-const PreBuildTask = `ls -l ${PROD_DOMAIN} && rm -rf  ${PROD_DOMAIN} && ls -l ${PROD_DOMAIN}`;
+const PreBuildTask = `ls -l ${common.OUTPUT_DIR} && rm -rf  ${common.OUTPUT_DIR} && ls -l ${common.OUTPUT_DIR}`;
 const PROD_DEPLOY_PATH = '/www';
-const PostBuildTask = `cp -r ${PROD_DOMAIN} ${PROD_DEPLOY_PATH}`;
+const PostBuildTask = `cp -r ${common.OUTPUT_DIR} ${PROD_DEPLOY_PATH}`;
 
-module.exports = function makeWebpackConfig() {
-  let config = {};
-  config.entry = {
-    app: './src/app/app.js'
-  };
 
-  config.output = {
-    path: __dirname + PROD_ROOT_PATH,
-    publicPath: PROD_ROOT_PATH,
-    filename: '[name].[hash].js',
-    chunkFilename: '[name].[hash].js'
-  };
+const OfflinePlugin = require('offline-plugin');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+module.exports = function() {
   config.devtool = 'source-map';
-
-  config.module = {
-    preLoaders: [],
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /node_modules/
-    }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')
-    }, 
-    {
-        test: /\.less$/,
-        loader: "style!css!less"
-      },
-      {
-      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-      loader: 'file'
-    }, {
-      test: /\.html$/,
-      loader: 'raw'
-    }, {
-      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "url-loader?minetype=application/font-woff"
-    }, {
-      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "file-loader"
-    },
-    {
-      test: /src\/public\/index\.html$/,
-      loader: 'string-replace',
-      query: {
-        search: '<base href="/">',
-        replace: `<base href="/${PROD_DOMAIN}/">`
-      }
-    }]
-  };
-  config.postcss = [
-    autoprefixer({
-      browsers: ['last 2 version']
-    })
-  ];
-
-  config.plugins = [];
-
+  // rewrite the base href in index.html
+  common.addLoader({
+    test: /src\/public\/index\.html$/,
+    loader: 'string-replace',
+    query: {
+      search: '<base href="/">',
+      replace: `<base href="/${common.OUTPUT_DIR}/">`
+    }
+  });
   config.plugins.push(
     new WebpackShellPlugin({
       onBuildStart: [PreBuildTask],
       onExit: [PostBuildTask]
     }),
-    new HtmlWebpackPlugin({
-      template: './src/public/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    }),
-
-    new ExtractTextPlugin('[name].[hash].css', {
-      disable: false
-    }),
-
+    common.htmlWebpackPlugin,
+    common.extractTextPlugin(false),
     new webpack.NoErrorsPlugin(),
-
     new webpack.optimize.DedupePlugin(),
-
     new webpack.optimize.UglifyJsPlugin(),
-
     new CopyWebpackPlugin(
       [{
         from: __dirname + '/src/public'
@@ -102,7 +43,7 @@ module.exports = function makeWebpackConfig() {
     new OfflinePlugin({
       // All options are optional
       caches: 'all',
-      scope: PROD_ROOT_PATH + '/',
+      scope: common.PUBLIC_PATH + '/',
       updateStrategy: 'all',
       version: 'v2',
 
@@ -114,6 +55,6 @@ module.exports = function makeWebpackConfig() {
         directory: 'appcache/'
       }
     })
-  )
+  );
   return config;
 }();
